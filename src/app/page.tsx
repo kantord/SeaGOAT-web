c6900c4b310ee15457dd4696e0108f6bacaf63be
Daniel Kantor <git@daniel-kantor.com>
2023-09-20 15:27:25 +0200

    feat: improve search bar formatting (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-24">
+    <main className="flex flex-col min-h-screen p-24 items-center">
       <input
         type="search"
         placeholder="Type here to search..."
         autoComplete="off"
         spellCheck="false"
+        className="h-12 max-w-lg w-full px-4 mb-4"
       />
     </main>
   );
