e0b07c9dd96b18aa49701adacf42b1859e92abd6
Daniel Kantor <git@daniel-kantor.com>
2023-09-20 16:32:09 +0200

    feat: display results when the user types

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./page";
 
-it("checks the search field functionality", async () => {
+it("shows results from SeaGOAT when typing into the search box", async () => {
   render(<Home />);
   const searchInput = screen.getByRole("searchbox");
   expect(searchInput).toBeInTheDocument();
-  fireEvent.change(searchInput, { target: { value: "Test search" } });
-  expect(searchInput).toHaveValue("Test search");
+
+  fireEvent.change(searchInput, {
+    target: { value: "where do we handle foobar" },
+  });
+  expect(searchInput).toHaveValue("where do we handle foobar");
+
+  const resultsWithFoobar = screen.getAllByText(/foobar/i);
+
+  resultsWithFoobar.forEach((article) => {
+    expect(article.closest("article")).toBeVisible();
+  });
+  expect(resultsWithFoobar.length).toBeGreaterThan(0);
+});
+
+it("does not display any results before the user searches for them", async () => {
+  render(<Home />);
+  const searchInput = screen.getByRole("searchbox");
+  expect(searchInput).toBeInTheDocument();
+
+  const resultsWithFoobar = screen.queryAllByText(/foobar/i);
+  expect(resultsWithFoobar.length).toEqual(0);
 });
