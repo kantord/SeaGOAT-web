da27041f668a218e913f73f8053ccbba775b424d
Daniel Kantor <git@daniel-kantor.com>
2023-09-20 12:30:13 +0200

    style: remove empyt lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen p-24">Hello World
-
-
       <button className="btn btn-primary">Button</button>
     </main>
   )
