368d56e506f156bd57a1d698bddaf68415020f72
Daniel Kantor <git@daniel-kantor.com>
2023-09-20 12:21:41 +0200

    feat: add daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-24">Hello World</main>
+    <main className="flex min-h-screen p-24">Hello World
+
+
+      <button className="btn btn-primary">Button</button>
+    </main>
   )
 }
