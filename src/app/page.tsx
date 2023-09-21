76643d2f033873cb3ec9a1978e3072150d2e950f
Daniel Kantor <git@daniel-kantor.com>
2023-09-21 12:44:12 +0200

    feat: display results when the user types (#7)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+
+import * as React from "react";
+
 export default function Home() {
+  const [queryText, setQueryText] = React.useState<string>("");
+  const resultTexts =
+    queryText.length > 0
+      ? [
+          "is is a foobar result",
+          "is is not a foobar result",
+          "is is another foobar result",
+        ]
+      : [];
+
   return (
     <main className="flex flex-col min-h-screen p-24 items-center">
       <input
@@ -7,7 +21,23 @@ export default function Home() {
         autoComplete="off"
         spellCheck="false"
         className="h-12 max-w-lg w-full px-4 mb-4"
+        value={queryText}
+        onChange={({ target }) => setQueryText(target.value)}
       />
+
+      <div className="w-full flex flex-col">
+        {resultTexts.map((text: string) => (
+          <article
+            className="card w-full bg-base-100 shadow-xl mb-4"
+            key={text}
+          >
+            <div className="card-body">
+              <h2 className="card-title">Card title!</h2>
+              <p>{text}</p>
+            </div>
+          </article>
+        ))}
+      </div>
     </main>
   );
 }
